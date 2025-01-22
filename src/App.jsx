f9064369a18d10cfa84ccc409a1f86e84cacc935
ExtraPolite <imextrapolite2@gmail.com>
2025-01-22 15:27:58 -0500

    add menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Link, useParams } from 'react-router';
 import Product from './Components/Product';
 import ShoppingCart from './Components/ShoppingCart';
 import Shop from './Components/Shop';
+import Menu from './Components/Menu';
 
 function App() {
   let { name } = useParams();
   const [getItems, setGetItem] = useState([]);
   const [getProduct, setGetProduct] = useState('');
   const [getCart, setCart] = useState([]);
+  const [clicked, setClicked] = useState(false);
 
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
@@ -21,14 +23,24 @@ function App() {
     setCart((prevItem) => [...prevItem, getProduct]);
   };
 
+  const handleClicked = () => {
+    if (clicked === false) {
+      setClicked(true);
+    } else {
+      setClicked(false);
+    }
+  };
+
   return (
     <>
       <nav>
+        <button onClick={handleClicked}>menu</button>
         <Link to="/">
           <h1>StoreName</h1>
         </Link>
         <Link to="/shopping-cart">Shopping cart</Link>
       </nav>
+      {clicked && <Menu />}
       <div>
         {name === 'product' ? (
           <Product showProduct={getProduct} addToCart={handleCart} />
---
+++ src/Components/Menu.jsx
@@ -0,0 +1,10 @@
+export default function Menu() {
+  return (
+    <div className="menu">
+      <button>Men</button>
+      <button>Women</button>
+      <button>Jewlery</button>
+      <button>Electronics</button>
+    </div>
+  );
+}
