8c728ced8b3d8a0fb6e2d6a7631a6f7875f66bd6
ExtraPolite <imextrapolite2@gmail.com>
2025-01-23 15:25:35 -0500

    create section for number of items to add to cart

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -3,6 +3,11 @@ export default function Product({ showProduct, addToCart }) {
     <div className="product">
       <img src={showProduct.image} alt={showProduct.title} />
       <h1>{showProduct.title}</h1>
+      <div className="product-increase">
+        <button>-</button>
+        <h2>1</h2>
+        <button>+</button>
+      </div>
       <button onClick={addToCart}>Add to Cart</button>
     </div>
   );
