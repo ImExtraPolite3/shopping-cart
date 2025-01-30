b5b5a547712d98f29e760e821c4c7508e4aed25b
ExtraPolite <imextrapolite2@gmail.com>
2025-01-29 22:52:06 -0500

    change delete button with svg

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -4,10 +4,18 @@ function ShoppingCart({ cart, removeCart }) {
       {cart.map((item, index) => {
         return (
           <div key={index}>
-            <img src={item[0].image} alt={item[0].title} />
-            <h3>{item[0].title}</h3>
-            <p>amount: {item[1]}</p>
-            <button onClick={() => removeCart(item)}>delete</button>
+            <div className="cart-image">
+              <img src={item[0].image} alt={item[0].title} />
+            </div>
+            <div className="cart-info">
+              <h3>{item[0].title}</h3>
+              <p> {`amount: ${item[1]}`}</p>
+              <img
+                src="/delete.svg"
+                alt="delete"
+                onClick={() => removeCart(item)}
+              />
+            </div>
           </div>
         );
       })}
