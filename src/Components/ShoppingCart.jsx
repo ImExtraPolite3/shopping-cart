217738efa2e682eeb826ffc39c468fa5734a28a9
ExtraPolite <imextrapolite2@gmail.com>
2025-01-31 14:12:11 -0500

    get price of all items. Work on getting price of multiple of the same item

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -1,27 +1,35 @@
 function ShoppingCart({ cart, removeCart }) {
+  let itemPrice = cart.reduce((acc, current) => acc + current[0].price, 0);
+
   return (
-    <div className="shopping-cart">
-      {cart.map((item, index) => {
-        return (
-          <div key={index}>
-            <div className="cart-image">
-              <img src={item[0].image} alt={item[0].title} />
-            </div>
-            <div className="cart-info">
-              <div>
-                <h4>{item[0].title}</h4>
-                <p> {`amount: ${item[1]}`}</p>
+    <>
+      <div className="shopping-cart">
+        {cart.map((item, index) => {
+          return (
+            <div key={index}>
+              <div className="cart-image">
+                <img src={item[0].image} alt={item[0].title} />
               </div>
-              <img
-                src="/delete.svg"
-                alt="delete"
-                onClick={() => removeCart(item)}
-              />
+              <div className="cart-info">
+                <div>
+                  <h4>{item[0].title}</h4>
+                  <p> {`amount: ${item[1]}`}</p>
+                </div>
+                <img
+                  src="/delete.svg"
+                  alt="delete"
+                  onClick={() => removeCart(item)}
+                />
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+        <div className="checkout">
+          <h3>${itemPrice}</h3>
+          <button>proceed to checkout</button>
+        </div>
+      </div>
+    </>
   );
 }
 
